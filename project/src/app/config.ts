e9aca40a140ef03738d77423eea77c6fadb0679c
mor747111 <mor.k.747111@gmail.com>
2017-04-16 19:37:28 +0300

    Added a config file including a colorPallete config

---
+++ project/src/app/config.ts
@@ -0,0 +1,15 @@
+export const config: object = {
+    GuidLength: 10,
+
+};
+
+export const colorPallete: object = {
+    primary: '#2196F3',
+    primary_dark: '#1976D2',
+    primary_light: '#BBDEFB',
+    accent: '#4CAF50',
+    primary_text: '#212121',
+    secondary_text: '#757575',
+    icons: '#FFFFFF',
+    divider: '#BDBDBD',
+}
