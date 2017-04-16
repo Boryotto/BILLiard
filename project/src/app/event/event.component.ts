79d51f13d8aae739aa5976f113e3920bb36d5fda
mor747111 <mor.k.747111@gmail.com>
2017-04-16 19:17:38 +0300

    Now the event component can get it's id from the path params

--- project/src/app/event/event.component.ts
+++ project/src/app/event/event.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'event',
   templateUrl: './event.template.html'
 })
-export class EventComponent { }
+export class EventComponent implements OnInit {
+
+  private eventId: number; // The event's Id
+
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.eventId = +params['id'];
+    })
+  }
+
+}
