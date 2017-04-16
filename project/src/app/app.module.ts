a895b6269c2cb4019cce086cbf0d7b5c4e54b653
mor747111 <mor.k.747111@gmail.com>
2017-04-16 14:34:26 +0300

    Added routing to the project and exported AppComponent's template to a seperate file

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
