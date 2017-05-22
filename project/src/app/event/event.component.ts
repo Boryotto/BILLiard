d5f44692454615c7b530259a00ccfba955f950f0
mor747111 <mor.k.747111@gmail.com>
2017-05-23 00:28:42 +0300

    Added options to the event page; Removed redundant code

--- project/src/app/event/event.component.ts
+++ project/src/app/event/event.component.ts
@@ -43,25 +43,6 @@ export class EventComponent implements OnInit {
     this.dataStorer.getGameEvent(this.eventId).then(resolvedEvent => this.event = resolvedEvent).catch(reason => {
       this.router.navigate(['/error'])
     });
-
-    // let eventName = "New Event";
-    // let tables: Table[] = [
-    //   new Table(1003, 'Table 23', 56.90, new Date(2017, 3, 25, 22, 0, 0, 0), null, true, TableMoveStatus.NULL),
-    //   new Table(1023, 'Table 53', 56.90, null, null, false, TableMoveStatus.NULL)
-    // ];
-    // let players: Player[] = [
-    //   new Player(50069, 'Mor Koshokaro', [new MiscItem(5556, "Weinstepfen Beer", 35.50, new Date(2017, 4, 2, 23, 0, 0, 0))]),
-    //   new Player(504069, 'Maya Levy', []),
-    //   new Player(503069, 'Lior Sarfati', []),
-    //   new Player(507069, 'Yuval Milman', []),
-    //   new Player(506069, 'Gadiel Ben Shitrit', []),
-    //   new Player(505069, 'Tal Zehazi', []),
-    // ];
-    // let movements: TableMovement[] = [];
-    // let tableRecords: TableRecord[] = [new TableRecord(80, tables[0], players[0], new Date(2017, 3, 25, 22, 0, 0, 0), null)]
-    // this.event = new GameEvent(1030, eventName, tables, players, 156, movements, tableRecords, new Date(), null);
-
-    // this.dataStorer.storeGameEvent(this.event);
   }
 
   private onChangeName() {
