c52fca7f69cf5f967dee1080498ff36c3abc51e7
mor747111 <mor.k.747111@gmail.com>
2017-06-12 23:31:03 +0300

    Fixed a crucial bug in the way the bill was calculated. Now each player pays for his time on the table + dead time divided evenly for all the players

--- project/src/app/services/game/money-calculations.service.ts
+++ project/src/app/services/game/money-calculations.service.ts
@@ -23,6 +23,35 @@ export class MoneyCalculationsService {
         if (table.start == undefined) {
             return 0;
         }
+        let leaseTimeMillis: number = this.calculateOverallTablePlayTimeMillis(table, event);
+        return (leaseTimeMillis / 3600000) * table.hourlyRate;
+    }
+
+    public calculatePlayerTableBill(player: Player, table: Table, event: GameEvent): number {
+        let playedTimeMillis = 0;
+        let totalTablePlayTimeMillis = 0;
+        event.tableRecords.filter(record => record.table.Id === table.Id).forEach(record => {
+            let recordPlayDuration;
+            if (record.end)
+                recordPlayDuration = record.end.getTime() - record.start.getTime();
+            else
+                recordPlayDuration = new Date().getTime() - record.start.getTime();
+
+            totalTablePlayTimeMillis += recordPlayDuration;
+            if (record.player.Id === player.Id)
+                playedTimeMillis += recordPlayDuration;
+        });
+        if (totalTablePlayTimeMillis === 0)
+            return 0;
+        else
+            return (playedTimeMillis / totalTablePlayTimeMillis) * this.calculateTotalTableBill(table, event);
+    }
+
+    private calculateTotalTableBill(table: Table, event: GameEvent): number {
+        return (this.calculateTableTotalPlayTimeMillis(table, event) / 3600000) * table.hourlyRate;
+    }
+
+    private calculateOverallTablePlayTimeMillis(table: Table, event: GameEvent): number {
         let leaseTimeMillis: number = 0;
         event.tableActivities.filter(activity => activity.table.Id === table.Id).forEach(activity => {
             if (activity.end != undefined) {
@@ -31,42 +60,64 @@ export class MoneyCalculationsService {
                 leaseTimeMillis += new Date().getTime() - activity.start.getTime();
             }
         });
-        return (leaseTimeMillis / 3600000) * table.hourlyRate;
+        return leaseTimeMillis;
     }
 
-    public calculatePlayerTableBill(player: Player, table: Table, event: GameEvent): number {
-        let records: TableRecord[] = event.tableRecords.filter(record =>
-            record.player.Id === player.Id
-            && record.table.Id === table.Id
-        );
-        let total: number = 0;
-        for (let record of records) {
-            let leaseTimeMillis: number = 0;
-            if (record.end != undefined) {
-                leaseTimeMillis = record.end.getTime() - record.start.getTime();
-            } else {
-                leaseTimeMillis = new Date().getTime() - record.start.getTime();
-            }
-            total += (leaseTimeMillis / 3600000) * record.table.hourlyRate;
+    // Calculates the total time players played on the given table for each table activity in milliseconds 
+    private calculateTableTotalPlayTimeMillis(table: Table, event: GameEvent): number {
+        let totalPlayTime = 0;
+        if (table && event) {
+
+            event.tableActivities.filter(activity => activity.table.Id === table.Id).
+                forEach(activity => {
+
+                    let records: TableRecord[] = event.tableRecords.filter(record => {
+                        if (record.table.Id === table.Id) {
+                            if (record.end && activity.end)
+                                return record.start.getTime() >= activity.start.getTime() && record.end.getTime() <= activity.end.getTime();
+                            else if (record.end && !activity.end)
+                                return record.start.getTime() >= activity.start.getTime();
+                            else if (!record.end && !activity.end)
+                                return true;
+                            else
+                                return false
+                        }
+                    });
+
+                    let recordStartTimes = records.map(record => record.start.getTime());
+                    let minStartTime = Math.min(...recordStartTimes);
+                    let recordEndTimes = records.map(record => {
+                        if (record.end)
+                            return record.end.getTime();
+                        else
+                            return new Date().getTime();
+                    });
+                    let maxEndTime = Math.max(...recordEndTimes);
+                    maxEndTime = maxEndTime === -Infinity ? 0 : maxEndTime;
+                    minStartTime = minStartTime === Infinity ? 0 : minStartTime;
+                    totalPlayTime += maxEndTime - minStartTime;
+                });
         }
-        player.miscItems.forEach(item => total += +item.price);
-        return total;
+        return totalPlayTime;
     }
 
     public calculatePlayerBill(player: Player, event: GameEvent): number {
-        let records: TableRecord[] = event.tableRecords.filter(record => record.player.Id === player.Id);
         let total: number = 0;
-        for (let record of records) {
-            let leaseTimeMillis: number = 0;
-            if (record.end != undefined) {
-                leaseTimeMillis = record.end.getTime() - record.start.getTime();
-            } else {
-                leaseTimeMillis = new Date().getTime() - record.start.getTime();
-            }
-            total += (leaseTimeMillis / 3600000) * record.table.hourlyRate;
-        }
+        event.tables.forEach(table => total += this.calculatePlayerTableBill(player, table, event));
         player.miscItems.forEach(item => total += +item.price);
+        total += event.players.length === 0 ? 0 : this.calculateDeadTimeBill(event) / event.players.length;
         return total;
+    }
+
+    public calculateDeadTimeBill(event: GameEvent): number {
+        let totalBill = 0;
+        event.tables.forEach(table => {
+            let totalTablePlayTime = this.calculateTableTotalPlayTimeMillis(table, event);
+            let overallPlayTime = this.calculateOverallTablePlayTimeMillis(table, event);
+            let totalDeadTime = overallPlayTime - totalTablePlayTime;
+            totalBill += (totalDeadTime / 3600000) * table.hourlyRate;
+        });
+        return totalBill;
     }
 
     public calculateTableRecordBill(tableRecord: TableRecord): number {
